14494b6d27292641d0b3aa2f55b4395bcfbaf28f
onlylove97 <songchichatdat97@gmail.com>
2017-10-17 21:39:51 +0700

    update search product on web

---
+++ PRJ321/src/java/com/entity/DemoEntity.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.entity;
+
+/**
+ *
+ * @author Admin
+ */
+public class DemoEntity {
+
+    private String productName;
+    private double unitPrice;
+    private String categoryName;
+    private String urlImage;
+
+    public DemoEntity(String productName, double unitPrice, String categoryName, String urlImage) {
+        this.productName = productName;
+        this.unitPrice = unitPrice;
+        this.categoryName = categoryName;
+        this.urlImage = urlImage;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public String getUrlImage() {
+        return urlImage;
+    }
+
+    public void setUrlImage(String urlImage) {
+        this.urlImage = urlImage;
+    }
+
+}
